a6381a900eed4b3942f14815bc6d0411f58c9349
luisaugusto <hello@luis.app>
2020-10-17 11:04:36 -0700

    added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+}
